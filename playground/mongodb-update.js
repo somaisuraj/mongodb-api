003ed3e4fdc53e488b976a0f4d97b061bf513c2c
somaisuraj <somai.suraj1990@gmail.com>
2019-01-12 20:33:11 +0900

    add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,17 @@
+const{MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',{useNewUrlParser: true}, (error, client) =>{
+  if (error) {
+     return console.log('unable to connect to mongodb server', error);
+  } console.log('connected to mongodb server');
+  const db = client.db('TodoApp');
+  db.collection('Users').findOneAndUpdate({_id: new ObjectID("5c38abdbdecf7a2fcc084364")}, {
+    $set: {location: 'NayaGau'},
+    $inc: {age: 10}
+    }, {
+    returnOriginal: true}).then((result) => {
+      console.log(result);
+    }, (error) => {
+      console.log(error);
+    });
+});
